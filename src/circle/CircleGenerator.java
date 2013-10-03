70921ba0b1e500a3375fcfc68b23da200d19a2bb
emakuta <gosha.egor.513@gmail.com>
2013-10-04 00:19:33 +0300

    test

--- src/circle/CircleGenerator.java
+++ src/circle/CircleGenerator.java
@@ -45,7 +45,7 @@ public class CircleGenerator {
 		result.add(new Point(x + dx,dy - y));
 		}
 		
-		while(y > limit)
+		while(y > limit) 
 		{
 			if (delta > 0)							// диагональная точка лежит вне окружности. нужно выбрать между вертикальным и диагональным пикселем
 			{
